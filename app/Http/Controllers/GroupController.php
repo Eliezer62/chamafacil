e9f8ebc87270914559d3579698a05791d952bcd8
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-09 13:36:44 -0300

    add getUsers para grupos

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Group;
+use App\Models\User;
 
 class GroupController extends Controller
 {
@@ -16,5 +17,11 @@ class GroupController extends Controller
     public function show(string $uuid)
     {
         return Group::find($uuid);
+    }
+
+
+    public function getUsers(string $uuid)
+    {
+        return User::where('group_id', $uuid)->get();
     }
 }
