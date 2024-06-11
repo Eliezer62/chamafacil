e652557d33f13d55b897bcb87c9f3c3dbaaf720f
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-10 23:57:39 -0300

    Rotas para react

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
 
-//Para que o laravel não trate rotas GET, apenas o React
+//Para que o laravel não trate rotas WEB, apenas o React
 Route::get( '/{path?}', function(){
     return view( 'app' );
 } )->where('path', '.*');
