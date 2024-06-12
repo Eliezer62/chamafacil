1eb9bd08cd3ee63775f8fdc4c0229a235620490f
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-11 21:23:00 -0300

    criado componente que vai a tela de abertura de chamados

---
+++ resources/js/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Auth from './Auth';
+import '../css/app.css';
+
+function Home()
+{
+
+    return (
+        <div className='container p-0 m-0'>
+            <nav className='nav-bar'>
+                <h3 className='align-center'>Novo chamado - ChamaFácil</h3>
+            </nav>
+            <div className="formulario">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Home;
