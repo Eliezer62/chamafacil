6678a147086a4baeff8cbdad7fe524045e98ba8a
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2024-06-13 18:26:15 -0300

    criado controller para categoria

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categoria;
+
+class CategoriaController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Categoria::all(), 200);
+    }
+}
